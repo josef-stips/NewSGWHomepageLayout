99b80c94dc1681100a8bd7e2b8a734ac12d71589
Josef Stips <stipsjosef@gmail.com>
2024-02-09 10:11:30 +0100

    init

---
+++ public/assets/readMe.txt
@@ -0,0 +1,3 @@
+replace 100x100 dummy image with sgw logo
+
+replace big dummy images with beautiful school photos
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+// set up
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static("public"));
+
+app.set('view engine', 'ejs');
+
+// routes (URI's)
+const userRouter = require("./routes/users.js");
+
+app.use('/users', userRouter);
+
+// listen to port
+app.listen(PORT, () => {
+    console.log(`api is running on port ${PORT}`);
+});
